b4846b5d5314a59662bbaba1e437aab0f482049a
Chetan Thakral <chetanthakral325#gmail.com>
2021-06-23 18:37:48 +0530

    5

--- Arrays/5.cpp
+++ Arrays/5.cpp
@@ -1,36 +1,51 @@
 //program:5
-//Move all the negative elements to one side of the array 
+//Move all the negative elements to one side of the array
 //without using constant extra space
-#include<bits/stdc++.h>
+//order of the elements after sorting does not matter
+#include <bits/stdc++.h>
 using namespace std;
-void move_negative_left(int arr[],int n){
-    int left=0;
-    int right=n-1;
-    while(left<=right){
-        if(arr[left]<0 && arr[right]<0){
+void move_negative_left(int arr[], int n)
+{
+    int left = 0;
+    int right = n - 1;
+    while (left <= right)
+    {
+        if (arr[left] < 0 && arr[right] < 0)
+        {
             left++;
-        }else if(arr[left]>0 && arr[right]<0){
-            swap(arr[left],arr[right]);
+        }
+        else if (arr[left] > 0 && arr[right] < 0)
+        {
+            swap(arr[left], arr[right]);
             left++;
             right--;
-        }else if(arr[left]>0 && arr[right]>0){
+        }
+        else if (arr[left] > 0 && arr[right] > 0)
+        {
             right--;
-        }else{
+        }
+        else
+        {
             left++;
             right--;
         }
     }
 }
-int main(){
+//start of the main function
+int main()
+{
     int n;
-    cin>>n;
+    cin >> n;
     int arr[n];
-    for(int i=0;i<n;i++){
-        cin>>arr[i];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
     }
-    move_negative_left(arr,n);
-    for(int i=0;i<n;i++){
-        cout<<arr[i]<<" ";
+    move_negative_left(arr, n);
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
     }
-
 }
+//time complexity:O(n)
+//auxiliary space:O(1)
